05ddc6cf30d7cd881d18987a9304c2427a9f0dae
13PizzaLover37 <kucheryavyi.valeriy@gmail.com>
2023-02-20 22:01:50 +0100

    Edit: changes at project and code clean up

--- ConsoleApp/ImportedObject/ImportedObject.cs
+++ ConsoleApp/ImportedObject/ImportedObject.cs
@@ -6,16 +6,11 @@ namespace ConsoleApp.ImportedObject
     {
         public string Name { get; set; }
         public string Schema;
-
         public string Type { get; set; }
-
-
         public string ParentName;
         public string ParentType { get; set; }
-
         public string DataType { get; set; }
         public string IsNullable { get; set; }
-
         public int NumberOfChildren;
     }
 }
