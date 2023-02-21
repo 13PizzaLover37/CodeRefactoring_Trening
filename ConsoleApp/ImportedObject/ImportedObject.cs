2c0699649467096b4eedd4880a61bd7fad9be9ed
13PizzaLover37 <kucheryavyi.valeriy@gmail.com>
2023-02-21 21:23:13 +0100

    Edit: changes at imported object

--- ConsoleApp/ImportedObject/ImportedObject.cs
+++ ConsoleApp/ImportedObject/ImportedObject.cs
@@ -5,12 +5,12 @@ namespace ConsoleApp.ImportedObject
     public class ImportedFile : IImportedObjectBase
     {
         public string Name { get; set; }
-        public string Schema;
+        public string Schema { get; set; }
         public string Type { get; set; }
-        public string ParentName;
+        public string ParentName { get; set; }
         public string ParentType { get; set; }
         public string DataType { get; set; }
         public string IsNullable { get; set; }
-        public int NumberOfChildren;
+        public int NumberOfChildren { get; set; }
     }
 }
